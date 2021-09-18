b7fa5a52cc3debf6315af506d1d6c470bb21023b
TheOnlyTails <theonlytails@theonlytails.com>
2021-09-18 21:34:08 +0300

    Added a search bar for blog posts. (#84)

--- src/lib/data/persistStore.js
+++ src/lib/data/persistStore.js
@@ -3,8 +3,7 @@ import { browser } from "$app/env"
 
 export const persistStore = (key, initial) => {
   if (browser) {
-    const persist =
-      localStorage.getItem(key) !== null ? localStorage.getItem(key) === "true" : initial
+    const persist = localStorage.getItem(key) !== null ? localStorage.getItem(key) : initial
     const store = writable(persist)
     store.subscribe((value) => localStorage.setItem(key, String(value)))
     return store
--- src/lib/data/persistStore.ts
+++ src/lib/data/persistStore.ts
@@ -1,10 +1,9 @@
 import { writable } from "svelte/store"
 import { browser } from "$app/env"
 
-export const persistStore = (key: string, initial: boolean) => {
+export const persistStore = <T>(key: string, initial: T) => {
   if (browser) {
-    const persist =
-      localStorage.getItem(key) !== null ? localStorage.getItem(key) === "true" : initial
+    const persist = localStorage.getItem(key) !== null ? localStorage.getItem(key) : initial
     const store = writable(persist)
     store.subscribe((value) => localStorage.setItem(key, String(value)))
 
