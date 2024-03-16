56dc616aa7db5daa9bc35436802f6fc1a14965b0
theonlytails <theonlytails@theonlytails.com>
2024-03-16 20:02:24 +0200

    better error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,22 +4,8 @@
 	import { Button } from "$lib/components/ui/button";
 </script>
 
-<main>
-	<h1>Error: Status Code {$page.status}</h1>
-	<p>{$page.error?.message}</p>
+<main class="h-dvh flex flex-col justify-center items-center gap-4">
+	<h1 class="font-semibold text-5xl">Error: Status Code {$page.status}</h1>
+	<p class="font-medium">{$page.error?.message}</p>
 	<Button variant="outline" href="/">Return Home</Button>
 </main>
-
-<style lang="postcss">
-	main {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		gap: 1rem;
-	}
-
-	h1 {
-		color: var(--accent);
-	}
-</style>
